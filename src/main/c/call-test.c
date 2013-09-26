8f1b58c609eb6fb37ac619dbca61cb7162f0663c
Paul Crowley <paul@lshift.net>
2013-09-26 11:59:58 +0100

    Fix call-test.c to use the header file

--- src/main/c/call-test.c
+++ src/main/c/call-test.c
@@ -1,33 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-#define CRYPTO_OUTPUTBYTES 64
-#define CRYPTO_INPUTBYTES 16
-#define CRYPTO_KEYBYTES 32
-#define CRYPTO_CONSTBYTES 16
-
-extern int crypto_core_salsa20_ref(
-        unsigned char *out,
-  const unsigned char *in,
-  const unsigned char *k,
-  const unsigned char *c
-);
-
+#include "crypto_core_salsa20.h"
 
 int main(
     int argc,
     const char *argv[]
 ) {
-    unsigned char sigma[CRYPTO_CONSTBYTES] = "expand 32-byte k";
-    unsigned char key[CRYPTO_KEYBYTES];
-    unsigned char input[CRYPTO_INPUTBYTES];
-    unsigned char output[CRYPTO_OUTPUTBYTES];
+    unsigned char sigma[crypto_core_salsa20_CONSTBYTES] = "expand 32-byte k";
+    unsigned char key[crypto_core_salsa20_KEYBYTES];
+    unsigned char input[crypto_core_salsa20_INPUTBYTES];
+    unsigned char output[crypto_core_salsa20_OUTPUTBYTES];
 
-    memset(key, 0, CRYPTO_KEYBYTES);
-    memset(input, 0, CRYPTO_INPUTBYTES);
+    memset(key, 0, crypto_core_salsa20_KEYBYTES);
+    memset(input, 0, crypto_core_salsa20_INPUTBYTES);
     key[0] = 0x80;
     crypto_core_salsa20(output, input, key, sigma);
-    for (int i = 0; i < CRYPTO_OUTPUTBYTES; i++) {
+    for (int i = 0; i < crypto_core_salsa20_OUTPUTBYTES; i++) {
         printf("%02x ", output[i]);
     }
     printf("\n");
